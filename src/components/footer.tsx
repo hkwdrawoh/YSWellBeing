d91e93df931ae163da1eac8e43f59a6e8e96910f
howardwkh <howardwan_8887@yahoo.com>
2024-03-24 17:29:52 +0800

    adjusted footer color

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -19,28 +19,11 @@ export default function Footer(props: {
       >
         <Collapsible.Trigger asChild>
           <div
-            className="bg-section h-10 flex justify-center px-2 cursor-pointer icon-hoverable"
+            className="bg-primary h-10 flex justify-center px-2 cursor-pointer icon-hoverable"
             title="Toggle device details"
           >
-            <div className="flex items-center justify-center gap-x-4 text-text font-light text-xs">
+            <div className="flex items-center justify-center gap-x-4 text-text1 font-light text-xs">
               <span>Toggle device information</span>
-              {props.open ? (
-                <Image
-                  src="/arrow-down.svg"
-                  width={20}
-                  height={20}
-                  alt="arrow-down"
-                  className="icon-hover-down"
-                />
-              ) : (
-                <Image
-                  src="/arrow-up.svg"
-                  width={20}
-                  height={20}
-                  alt="arrow-up"
-                  className="icon-hover-up"
-                />
-              )}
             </div>
           </div>
         </Collapsible.Trigger>
