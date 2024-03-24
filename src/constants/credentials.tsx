861414cb99b99843c6eb1b1172216fa75b2469e5
howardwkh <howardwan_8887@yahoo.com>
2024-03-24 22:25:35 +0800

    add credential details

---
+++ src/constants/credentials.tsx
@@ -0,0 +1,19 @@
+export const salt = "YSWellBeing"
+
+export const credential = [
+    {
+        // Admin Account, pid: 9999999, pw: admin
+        PID: "9999999",
+        PW_hash: "77ca8033c26ca6efe8a39ee8d68115f7bf2f93a0b89bca8eb0b27c4a73d6779f"
+    },
+    {
+        // Mr. Roger Chan, pid: 0002357, pw: chan90002357
+        PID: "0002357",
+        PW_hash: "ccb7643febcac9ffb097ce1ce27d19e95b6b090bc9c67624fe73991914781bb3"
+    },
+    {
+        // Ms. Natalie Lai, pid: 0004689, pw: lai90004689
+        PID: "0004689",
+        PW_hash: "531c682e1c1b4219c55763d515629551ead3aec7cbcc59322dde2b3e79873a43"
+    },
+]
