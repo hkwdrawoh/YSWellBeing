d5d86b1ceb5eb27a2fe9186a08aed099a1908635
howardwkh <howardwan_8887@yahoo.com>
2024-03-24 17:30:53 +0800

    add images to subscriber section

--- src/components/subscriber.tsx
+++ src/components/subscriber.tsx
@@ -5,6 +5,7 @@ import { prefetchConfig, registerServiceWorker } from "@magicbell/webpush"
 import subscriptionManager from "@/services/subscriptionManager"
 import Button from "@/components/button"
 import { State } from "@/pages"
+import Image from "next/image";
 
 export default function Subscriber({
   state,
@@ -53,24 +54,28 @@ export default function Subscriber({
   const isLoading = !subscribeOptions.token || state.status === "busy"
 
   if (isLoading) {
-    return <Button text="Loading" classname="bg-gray-500" disabled={true} />
+    return <>
+      <Image src="/notification.png" className={"inline-block my-6"} alt="bell" width={60} height={60} />
+      <Button text="Loading" classname="bg-gray-500" disabled={true} />
+      <p className="text-xs mt-6 mb-16">
+        Please allow the app to send notifications to remind taking prescription on time.
+      </p>
+    </>
   }
 
   if (state.status === "error") {
-    return <Button text="Error" classname="bg-red-400" disabled={true} />
+    return <>
+      <Image src="/notification.png" className={"inline-block my-6"} alt="bell" width={60} height={60} />
+      <Button text="Error" classname="bg-red-400" disabled={true} />
+    </>
   }
 
   return (
     <>
-      <Button
-        onClick={handleSubscribe}
-        text="Subscribe"
-        classname="bg-primary"
-        disabled={false}
-      />
+      <Image src="/notification.png" className={"inline-block my-6"} alt="bell" width={60} height={60} />
+      <Button onClick={handleSubscribe} text="Allow Notification" classname="bg-primary" disabled={false} />
       <p className="text-xs mt-6 mb-16">
-        * Once you subscribe we will send you one automatic test-notification.
-        You can unsubscribe at any time.
+        Please allow the app to send notifications to remind taking prescription on time.
       </p>
     </>
   )
